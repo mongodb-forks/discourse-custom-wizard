c2a380945dbf5fb5edafdfd689febaf638dfec09
timmsantos <timmsantos@icloud.com>
2022-07-27 10:28:29 +1000

    custom wizard fix

--- assets/javascripts/wizard-custom-guest.js
+++ assets/javascripts/wizard-custom-guest.js
@@ -1,6 +1,7 @@
 (function () {
+    
   document.cookie = "destination_url=" + window.location.href + ";path=/";
   let url = window.location.href;
   let split_url = url.split('/w/');
-  window.location.replace(split_url[0] + "/login");
+  window.location.replace(split_url[0] + "/auth/saml/?origin=" + url);
 })();
